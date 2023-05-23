e2c5328b27d33fa280534341bd59f0b2b541f98c
Pulasthi66 <thssaranath@gmail.com>
2023-05-23 18:25:20 +0530

    Main Menu Programing
    working MainMenu with Programing

---
+++ Assets/Scripts/UI_Scripts/MENU_UI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENU_UI : MonoBehaviour
+{
+    [SerializeField] private GameObject _MainMenu_Panal;
+    [SerializeField] private GameObject _LevelSelect_Panal;
+    [SerializeField] private GameObject _Setings_Panal;
+    [SerializeField] private GameObject _Credits_Panal;
+
+
+    public void play()
+    {
+        _MainMenu_Panal.SetActive(false);
+        _LevelSelect_Panal.SetActive(true);
+       
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    public void back()
+    {
+        _MainMenu_Panal.SetActive(true);
+        _Credits_Panal.SetActive(false);
+        _LevelSelect_Panal.SetActive(false);
+        _Setings_Panal.SetActive(false);
+    }
+    public void settings()
+    {
+        _Setings_Panal.SetActive(true);
+        _MainMenu_Panal.SetActive(false);
+    }
+    public void credits()
+    {
+        _Credits_Panal.SetActive(true);
+        _MainMenu_Panal.SetActive(false);
+    }
+
+}
