9f408b0f901c31e9a0389a46264c6e7e0db9f926
Pulasthi66 <thssaranath@gmail.com>
2023-05-26 21:31:52 +0530

    make level select menu
    working level select menu & animation for UI

---
+++ Assets/Scripts/UI_Scripts/LoladLevels_UI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoladLevels_UI : MonoBehaviour
+{
+    public void loadLevel_1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void loadLevel_2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void loadLevel_3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void loadLevel_4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void loadLevel_5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void loadLevel_6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void loadLevel_7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void loadLevel_8()
+    {
+        SceneManager.LoadScene(8);
+    }
+}
