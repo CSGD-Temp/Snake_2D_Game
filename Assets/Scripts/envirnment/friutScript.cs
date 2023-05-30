518997aeb0e2c6ca59175cb95daf1dc24a6e8ee2
Parakkrama24 <110714055+Parakkrama24@users.noreply.github.com>
2023-05-31 01:49:35 +0530

    Parakkrama
    Level painting and level bug fix

---
+++ Assets/Scripts/envirnment/friutScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class friutScript : MonoBehaviour
+{
+    [Header("Scripts")]
+    private friutspwner _friutspwner;
+
+    private void Start()
+    {
+        GameObject friutspwner = GameObject.FindGameObjectWithTag("FriutSpawner");
+        _friutspwner=friutspwner.GetComponent<friutspwner>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("collide");
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(this.gameObject);
+            _friutspwner.friutSpawner();
+        }
+    }
+
+}
