34bb4d8b08f68c852a5cac316a2d9adacf5cf951
Pulasthi66 <thssaranath@gmail.com>
2023-05-30 17:30:38 +0530

    Setting screen programing

---
+++ Assets/Scripts/UI_Scripts/Setings_UI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Setings_UI : MonoBehaviour
+{
+    [SerializeField] private AudioMixer _aMixer;
+    [SerializeField] private Slider _Music;
+    [SerializeField] private Slider _Sounds;
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("GAME_Sounds"))
+        {
+            _aMixer.SetFloat("GAME_Sounds", 0f);
+            _aMixer.SetFloat("PLAYER_Sounds", 0f);
+        }
+        else LOAD();
+    }
+
+    public void GameMusic()
+    {
+        _aMixer.SetFloat("GAME_Sounds", _Music.value);
+    }
+
+    public void PlayerSounds()
+    {
+        _aMixer.SetFloat("PLAYER_Sounds", _Sounds.value);
+    }
+
+    void LOAD()
+    {
+        _Music.value = PlayerPrefs.GetFloat("GAME_Sounds");
+        _Sounds.value = PlayerPrefs.GetFloat("PLAYER_Sounds");
+    }
+    public void SAVE()
+    {
+        PlayerPrefs.SetFloat("GAME_Sounds", _Music.value);
+        PlayerPrefs.SetFloat("PLAYER_Sounds", _Sounds.value);
+    }
+}
