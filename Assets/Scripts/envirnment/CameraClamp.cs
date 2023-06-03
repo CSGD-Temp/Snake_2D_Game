a0ae9a78c72c10fccb83e6f9104ce1378dd95680
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2023-06-03 20:54:17 +0530

    Camara clamp
    Using script camara clamp camara cant go out of boundaries

---
+++ Assets/Scripts/envirnment/CameraClamp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraClamp : MonoBehaviour
+{
+    public Transform target;
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
+
+    private void LateUpdate()
+    {
+        if (target != null)
+        {
+            // Get the camera's position
+            Vector3 newPosition = transform.position;
+
+            // Clamp the camera's position within the defined bounds
+            newPosition.x = Mathf.Clamp(target.position.x, minBounds.x, maxBounds.x);
+            newPosition.y = Mathf.Clamp(target.position.y, minBounds.y, maxBounds.y);
+
+            // Update the camera's position
+            transform.position = newPosition;
+        }
+    }
+}
